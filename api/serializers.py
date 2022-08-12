438cb13e5e3eb6837f771dfeeb993531b6fcc3c6
jithin20 <jithinvv4u@gmail.com>
2022-08-12 10:13:03 +0530

    updated serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,4 @@ from .models import ToDo
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model=ToDo
-        fields=('id','title','description','completed')
+        fields=('id','title','description','completed','user')
